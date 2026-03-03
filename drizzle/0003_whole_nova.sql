2a81ff54bc56ed30d0fa50f5e3fa8d036120c7c1
Aina84 <johnsamuelaina84@gmail.com>
2026-03-03 21:40:55 +0100

    feat : migration for report entity

---
+++ drizzle/0003_whole_nova.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `sheeps` ADD `status` text;
--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -4,13 +4,15 @@ import journal from './meta/_journal.json';
 import m0000 from './0000_keen_kid_colt.sql';
 import m0002 from './0002_public_sabra.sql';
 import m0003 from './0003_whole_nova.sql';
+import m0004 from './0004_natural_shape.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
 m0002,
-m0003
+m0003,
+m0004
     }
   }
   
